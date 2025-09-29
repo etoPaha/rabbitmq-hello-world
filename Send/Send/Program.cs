8be2238c4eda0c7f761125e72a1dcf27354a6386
berezkin.pavel <cheliabkns@gmail.com>
2025-09-30 07:39:01 +1000

    2 проекта и наброски Send

---
+++ Send/Send/Program.cs
@@ -0,0 +1,19 @@
+﻿using System.Text;
+using RabbitMQ.Client;
+
+// создаем канал
+// создаем очередь
+
+var factory = new ConnectionFactory
+{
+    HostName = "localhost"
+};
+using var connection = await factory.CreateConnectionAsync();
+using var channel = await connection.CreateChannelAsync();
+
+await channel.QueueDeclareAsync(queue: "hello", durable: false, exclusive: false, autoDelete: false, arguments: null);
+
+const string message = "Hello World!";
+var body = Encoding.UTF8.GetBytes(message);
+
+Console.ReadLine();
