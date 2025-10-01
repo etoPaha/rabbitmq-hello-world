b16ee43075484f7715bfceb72b6c5bf28bb2b279
berezkin.pavel <cheliabkns@gmail.com>
2025-10-01 22:45:08 +1000

    сделал примеры с простой отправкой и получением

--- Send/Send/Program.cs
+++ Send/Send/Program.cs
@@ -11,9 +11,22 @@ var factory = new ConnectionFactory
 using var connection = await factory.CreateConnectionAsync();
 using var channel = await connection.CreateChannelAsync();
 
+// объявляем очередь hello
 await channel.QueueDeclareAsync(queue: "hello", durable: false, exclusive: false, autoDelete: false, arguments: null);
 
-const string message = "Hello World!";
-var body = Encoding.UTF8.GetBytes(message);
+while (true)
+{
+    Console.WriteLine(@" ""e"" для выхода / [enter] отправить сообщение ");
+    var line = Console.ReadLine();
+    if (line == "e")
+        break;
+    
+    const string message = "Hello World!";
+    var body = Encoding.UTF8.GetBytes(message);
 
+    await channel.BasicPublishAsync(exchange: "", routingKey: "hello", body: body);
+    Console.WriteLine($" [x] Sent {message}");
+}
+
+Console.WriteLine(" Press [enter] to exit. ");
 Console.ReadLine();
